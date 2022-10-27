9c4f80ef636a076c45a0d3fc9d9cca849705dc03
sergiop45 <sergiopsantos33@gmail.com>
2022-10-27 08:25:57 -0300

    user-resource-and-router-create

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { User } from "../entities/user.entity";
+
+export class CreateUserDto extends User{
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(25)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'password too weak',
+      })
+    password: string;
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService){}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const data: Prisma.UserCreateInput = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+
+    const createdUser = await this.prisma.user.create({data});
+
+    return {
+      ...createdUser,
+      password: undefined,
+    }
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
